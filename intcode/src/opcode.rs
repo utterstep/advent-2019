b65a35cdb1b1fc4740f473e596adf65de208812e
Vlad Stepanov <8uk.8ak@gmail.com>
2020-01-03 00:22:56 +0300

    [opt] hand unrollment of ParameterMode parsing

--- intcode/src/opcode.rs
+++ intcode/src/opcode.rs
@@ -28,6 +28,26 @@ pub(crate) struct Opcode {
 
 impl From<i64> for Opcode {
     fn from(mut value: i64) -> Self {
+        if value < 100 {
+            let parameter_modes = [ParameterMode::Position; 3];
+
+            let operation = match value {
+                1 => Operation::Add,
+                2 => Operation::Multiply,
+                3 => Operation::Input,
+                4 => Operation::Output,
+                5 => Operation::JumpIfTrue,
+                6 => Operation::JumpIfFalse,
+                7 => Operation::LessThan,
+                8 => Operation::Equals,
+                9 => Operation::AdjRelBase,
+                99 => Operation::End,
+                _ => Operation::Unknown,
+            };
+
+            return Self { parameter_modes, operation };
+        }
+
         let operation = match value % 100 {
             1 => Operation::Add,
             2 => Operation::Multiply,
@@ -42,27 +62,42 @@ impl From<i64> for Opcode {
             _ => Operation::Unknown,
         };
 
-        let mut parameter_modes = [ParameterMode::Position; 3];
-
         value /= 100;
-        let mut i = 0;
 
-        while value > 0 {
-            parameter_modes[i] = match value % 10 {
-                0 => ParameterMode::Position,
-                1 => ParameterMode::Immediate,
-                2 => ParameterMode::Relative,
-                _ => {
-                    return Self {
-                        operation: Operation::Unknown,
-                        parameter_modes,
-                    };
-                }
-            };
-
-            i += 1;
-            value /= 10;
-        }
+        // my eyes :( but this unrollment gives like +20% perf on day-9 (4ms vs 5ms on turbo-locked Core i7-8750H)
+        let parameter_modes = match value {
+            000 => [ParameterMode::Position, ParameterMode::Position, ParameterMode::Position],
+            001 => [ParameterMode::Immediate, ParameterMode::Position, ParameterMode::Position],
+            002 => [ParameterMode::Relative, ParameterMode::Position, ParameterMode::Position],
+            010 => [ParameterMode::Position, ParameterMode::Immediate, ParameterMode::Position],
+            011 => [ParameterMode::Immediate, ParameterMode::Immediate, ParameterMode::Position],
+            012 => [ParameterMode::Relative, ParameterMode::Immediate, ParameterMode::Position],
+            020 => [ParameterMode::Position, ParameterMode::Relative, ParameterMode::Position],
+            021 => [ParameterMode::Immediate, ParameterMode::Relative, ParameterMode::Position],
+            022 => [ParameterMode::Relative, ParameterMode::Relative, ParameterMode::Position],
+            100 => [ParameterMode::Position, ParameterMode::Position, ParameterMode::Immediate],
+            101 => [ParameterMode::Immediate, ParameterMode::Position, ParameterMode::Immediate],
+            102 => [ParameterMode::Relative, ParameterMode::Position, ParameterMode::Immediate],
+            110 => [ParameterMode::Position, ParameterMode::Immediate, ParameterMode::Immediate],
+            111 => [ParameterMode::Immediate, ParameterMode::Immediate, ParameterMode::Immediate],
+            112 => [ParameterMode::Relative, ParameterMode::Immediate, ParameterMode::Immediate],
+            120 => [ParameterMode::Position, ParameterMode::Relative, ParameterMode::Immediate],
+            121 => [ParameterMode::Immediate, ParameterMode::Relative, ParameterMode::Immediate],
+            122 => [ParameterMode::Relative, ParameterMode::Relative, ParameterMode::Immediate],
+            200 => [ParameterMode::Position, ParameterMode::Position, ParameterMode::Relative],
+            201 => [ParameterMode::Immediate, ParameterMode::Position, ParameterMode::Relative],
+            202 => [ParameterMode::Relative, ParameterMode::Position, ParameterMode::Relative],
+            210 => [ParameterMode::Position, ParameterMode::Immediate, ParameterMode::Relative],
+            211 => [ParameterMode::Immediate, ParameterMode::Immediate, ParameterMode::Relative],
+            212 => [ParameterMode::Relative, ParameterMode::Immediate, ParameterMode::Relative],
+            220 => [ParameterMode::Position, ParameterMode::Relative, ParameterMode::Relative],
+            221 => [ParameterMode::Immediate, ParameterMode::Relative, ParameterMode::Relative],
+            222 => [ParameterMode::Relative, ParameterMode::Relative, ParameterMode::Relative],
+            _ => return Self {
+                operation: Operation::Unknown,
+                parameter_modes: [ParameterMode::Position; 3],
+            },
+        };
 
         Self {
             operation,
